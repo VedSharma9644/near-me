221b86172c41faf2880c1efe26e8af309ab815a0
VedSharma9644 <58023735+VedSharma9644@users.noreply.github.com>
2025-02-10 05:52:57 +0530

    Try commit 2

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,8 @@ const app = express();
 app.use(express.json());
 
 // Enable CORS to allow communication between front-end and back-end
-app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3002"], })); // Adjust the origin if needed
-
+// app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3002"], })); // Adjust the origin if needed
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
